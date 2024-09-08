3232c2a5b73351af16338527ba9f8378d06fde69
gunita27 <148987784+gunita27@users.noreply.github.com>
2024-09-08 06:34:42 +0000

    guess game learned if else conditions user input and changing string into integer using parse and used rand crate. It's Really fun to learn new languages honestly

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+use rand::Rng;//rand is a crate that provides the random number generator
 use std::io;
 fn main() {
+    let correct = rand::thread_rng().gen_range(1..=10);
+    println!("correct:{correct}");
     println!("Hello there ");
-    println!("Guesss a number");
-    let mut name=String::new();
-    io::stdin().read_line(&mut name).expect("error reading input");
-    println!("You guessed {name} number");
-    println!("You guessed {} number",name.trim());
+    println!("Guesss a number from 1-10");
+    let mut guess = String::new();
+//std is standard library
+    io::stdin()
+        .read_line(&mut guess)//read_line is used to read the input from the user
+        .expect("error reading input");
+  let guess: u32 = guess.trim().parse().expect("error with parse");//parse is used to convert string to integer
+  //expect is used to handle error
+    if correct >guess{
+        println! (
+          
+          "you picked smaller number"
+        );}
+        else if correct < guess {
+          println!(
+            "you picked greater number"
+          );
+        
+    }else{
+        println!("you chose the correct number");
+    }
+  
+    // println!("You guessed {} number", name.trim());//trim cant be used with integer its for string
 }
